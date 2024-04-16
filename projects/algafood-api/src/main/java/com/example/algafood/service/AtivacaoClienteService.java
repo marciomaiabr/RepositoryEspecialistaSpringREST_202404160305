3c51aa3793737480cc6772f924d3f6be4249249c
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-16 11:17:12 -0400

    Criando métodos de callback do ciclo de vida dos beans

--- projects/algafood-api/src/main/java/com/example/algafood/service/AtivacaoClienteService.java
+++ projects/algafood-api/src/main/java/com/example/algafood/service/AtivacaoClienteService.java
@@ -8,8 +8,24 @@ import com.example.algafood.notificacao.INotificador;
 import com.example.algafood.qualifiers.NivelUrgencia;
 import com.example.algafood.qualifiers.TipoDoQualificador;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class AtivacaoClienteService {
+
+	static {
+		System.out.println("AtivacaoClienteService.static");
+	}
+
+	public AtivacaoClienteService() {
+		System.out.println("AtivacaoClienteService.AtivacaoClienteService()");
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("AtivacaoClienteService.postConstruct()");
+	}
 
 	@Autowired
 	@TipoDoQualificador(NivelUrgencia.URGENTE)
@@ -18,6 +34,11 @@ public class AtivacaoClienteService {
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		notificador.notificar(cliente, "Seu cadastro está ativo !");
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+		System.out.println("AtivacaoClienteService.preDestroy()");
 	}
 
 }
