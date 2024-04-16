479161b559a9208aafcf2b89c74672dfe23f9947
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-16 06:29:10 -0400

    Injetando dependências (beans Spring)

--- projects/algafood-api/src/main/java/com/example/algafood/service/AtivacaoClienteService.java
+++ projects/algafood-api/src/main/java/com/example/algafood/service/AtivacaoClienteService.java
@@ -2,11 +2,26 @@ package com.example.algafood.service;
 
 import org.springframework.stereotype.Component;
 
+import com.example.algafood.modelo.Cliente;
+import com.example.algafood.notificacao.NotificadorEmail;
+
 @Component
 public class AtivacaoClienteService {
 
-	public AtivacaoClienteService() {
+	private NotificadorEmail notificadorEmail;
+
+	/*public AtivacaoClienteService() {
 		System.out.println("AtivacaoClienteService.AtivacaoClienteService()");
+	}*/
+
+	public AtivacaoClienteService(NotificadorEmail notificadorEmail) {
+		System.out.println("AtivacaoClienteService.AtivacaoClienteService(NotificadorEmail)[" + notificadorEmail + "]");
+		this.notificadorEmail = notificadorEmail;
 	}
-	
+
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+		notificadorEmail.notificar(cliente, "Seu cadastro está ativo !");
+	}
+
 }
