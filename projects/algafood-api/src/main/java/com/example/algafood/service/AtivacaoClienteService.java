ed01faefda70969714c6a58ad504f3bdd6ace47e
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-16 13:33:12 -0400

    Publicando e consumindo eventos customizados

--- projects/algafood-api/src/main/java/com/example/algafood/service/AtivacaoClienteService.java
+++ projects/algafood-api/src/main/java/com/example/algafood/service/AtivacaoClienteService.java
@@ -1,12 +1,10 @@
 package com.example.algafood.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import com.example.algafood.modelo.Cliente;
-import com.example.algafood.notificacao.INotificador;
-import com.example.algafood.qualifiers.NivelUrgencia;
-import com.example.algafood.qualifiers.TipoDoQualificador;
 
 @Component
 public class AtivacaoClienteService {
@@ -20,12 +18,11 @@ public class AtivacaoClienteService {
 	}
 
 	@Autowired
-	@TipoDoQualificador(NivelUrgencia.URGENTE)
-	private INotificador notificador;
+	private ApplicationEventPublisher applicationEventPublisher;
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
-		notificador.notificar(cliente, "Seu cadastro está ativo !");
+		applicationEventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
 	}
 
 }
