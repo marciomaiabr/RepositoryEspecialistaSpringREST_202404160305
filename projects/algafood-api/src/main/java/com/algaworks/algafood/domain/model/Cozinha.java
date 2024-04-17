5db6524840b6bc5ef34c9249da73c8d477264ce5
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-17 06:42:14 -0400

    Conhecendo e usando o Lombok

--- projects/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ projects/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -4,16 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@EqualsAndHashCode
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
