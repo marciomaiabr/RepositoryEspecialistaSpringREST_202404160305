a7c3931280a98cccbd2df943a9df0ab4bd10deba
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-17 06:10:36 -0400

    Conhecendo e implementando o padrão Repository

--- projects/algafood-api/src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
+++ projects/algafood-api/src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
@@ -8,6 +8,7 @@ import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 public class ConsultaCozinhaMain {
 
@@ -15,9 +16,9 @@ public class ConsultaCozinhaMain {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
 				.web(WebApplicationType.NONE).run(args);
 
-		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		CozinhaRepository cozinhaRepository = applicationContext.getBean(CozinhaRepository.class);
 
-		List<Cozinha> cozinhas = cadastroCozinha.listar();
+		List<Cozinha> cozinhas = cozinhaRepository.listar();
 
 		for (Cozinha cozinha : cozinhas)
 			System.out.println(cozinha.getNome());
