ded485f904f18ec9b81cedbd7e8cb0769e3bcff9
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-17 04:19:43 -0400

    Importando dados de teste com import.sql

--- projects/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ projects/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,21 +1,17 @@
 package com.algaworks.algafood.domain.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tab_cozinhas")
 public class Cozinha {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "nom_cozinha")
 	private String nome;
 
 	public Long getId() {
