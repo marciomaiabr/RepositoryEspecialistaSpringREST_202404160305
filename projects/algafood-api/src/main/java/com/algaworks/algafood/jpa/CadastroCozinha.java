bf6170a6f95858eb3d5bee1d47298b2ecc8c4575
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-17 05:31:16 -0400

    Excluindo um objeto do banco de dados

--- projects/algafood-api/src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ projects/algafood-api/src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -29,4 +29,10 @@ public class CadastroCozinha {
 		return manager.merge(cozinha);
 	}
 
+	@Transactional
+	public void remover(Cozinha cozinha) {
+		cozinha = buscar(cozinha.getId());
+		manager.remove(cozinha);
+	}
+
 }
