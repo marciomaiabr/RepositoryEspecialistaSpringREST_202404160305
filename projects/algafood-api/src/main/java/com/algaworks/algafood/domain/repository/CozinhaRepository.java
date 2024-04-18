5b157f82d2af66a516058524acb4b07bab16cc42
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-18 07:18:59 -0400

    Implementando consultas JPQL em repositórios

--- projects/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ projects/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -2,12 +2,18 @@ package com.algaworks.algafood.domain.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.domain.model.Cozinha;
 
+@Component
 public interface CozinhaRepository {
 
 	List<Cozinha> listar();
+	List<Cozinha> consultarPorNome(String nome);
 	Cozinha buscar(Long id);
+	
+	
 	Cozinha salvar(Cozinha cozinha);
 	void remover(Long id);
 	
