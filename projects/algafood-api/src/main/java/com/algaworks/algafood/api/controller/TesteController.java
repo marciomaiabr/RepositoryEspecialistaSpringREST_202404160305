dd1d673818532a758838aa4e5299eb653a91c936
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-18 10:02:09 -0400

    Criando consultas com query methods

--- projects/algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ projects/algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,11 @@ public class TesteController {
 	@GetMapping("/cozinhas/por-nome")
 	public List<Cozinha> cozinhasPorNome(String nome) {
 		return cozinhaRepository.findTodasByNome(nome);
+	}
+
+	@GetMapping("/cozinhas/unico-por-nome")
+	public Optional<Cozinha> cozinhaPorNome(String nome) {
+		return cozinhaRepository.findByNome(nome);
 	}
 
 }
--- projects/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ projects/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
 	List<Cozinha> findTodasByNome(String nome);
+
+	Optional<Cozinha> findByNome(String nome);
 
 }
