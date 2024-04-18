4cc394af5fb16910eb05f7c5a001db7d6119307b
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-18 14:45:28 -0400

    Tornando a consulta com Criteria API com filtros dinâmicos

--- projects/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ projects/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -17,10 +16,10 @@ public interface RestauranteRepository
 
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
-	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
-	List<Restaurante> consultaPorNome(String nome, @Param("id") Long cozinha);
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 	
-	//List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+//	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
 	
 	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
 	
--- projects/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ projects/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,12 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -23,32 +23,28 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@Override
 	public List<Restaurante> find(String nome, 
 			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		var builder = manager.getCriteriaBuilder();
 		
-		var jpql = new StringBuilder();
-		jpql.append("from Restaurante where 0 = 0 ");
+		var criteria = builder.createQuery(Restaurante.class);
+		var root = criteria.from(Restaurante.class);
+
+		var predicates = new ArrayList<Predicate>();
 		
-		var parametros = new HashMap<String, Object>();
-		
-		if (StringUtils.hasLength(nome)) {
-			jpql.append("and nome like :nome ");
-			parametros.put("nome", "%" + nome + "%");
+		if (StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
 		}
 		
 		if (taxaFreteInicial != null) {
-			jpql.append("and taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaFreteInicial);
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
 		}
 		
 		if (taxaFreteFinal != null) {
-			jpql.append("and taxaFrete <= :taxaFinal ");
-			parametros.put("taxaFinal", taxaFreteFinal);
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
 		}
 		
-		TypedQuery<Restaurante> query = manager
-				.createQuery(jpql.toString(), Restaurante.class);
+		criteria.where(predicates.toArray(new Predicate[0]));
 		
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-
+		var query = manager.createQuery(criteria);
 		return query.getResultList();
 	}
 	
