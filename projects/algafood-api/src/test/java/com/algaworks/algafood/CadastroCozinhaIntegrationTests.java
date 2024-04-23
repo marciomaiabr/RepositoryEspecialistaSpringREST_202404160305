6f54890df18b6b22da7f69a346ba1d81dcc567e3
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-23 13:31:10 -0400

    Desafio: escrevendo testes de integração

--- projects/algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ projects/algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -6,41 +6,55 @@ import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import javax.validation.ConstraintViolationException;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class CadastroCozinhaIntegrationTests {
+public class CadastroCozinhaIntegrationTests {
 
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
-	
+
 	@Test
-	public void testarCadastroCozinhaComSucesso() {
-		// cenário
+	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome("Chinesa");
-		
-		// ação
+
 		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-		
-		// validação
+
 		assertThat(novaCozinha).isNotNull();
 		assertThat(novaCozinha.getId()).isNotNull();
 	}
 
 	@Test
-	public void testarCadastroCozinhaSemNome() {
-	    assertThrowsExactly(ConstraintViolationException.class,
-	            ()->{
-	        		Cozinha novaCozinha = new Cozinha();
-	        		novaCozinha.setNome(null);
-	        		
-	        		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-	            });
+	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
+		assertThrowsExactly(ConstraintViolationException.class, () -> {
+			Cozinha novaCozinha = new Cozinha();
+			novaCozinha.setNome(null);
+			novaCozinha = cadastroCozinha.salvar(novaCozinha);
+		});
 	}
-	
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		assertThrowsExactly(EntidadeEmUsoException.class, () -> {
+			cadastroCozinha.excluir(1L);
+		});
+	}
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		assertThrowsExactly(CozinhaNaoEncontradaException.class, () -> {
+			cadastroCozinha.excluir(100L);
+		});
+	}
+
 }
