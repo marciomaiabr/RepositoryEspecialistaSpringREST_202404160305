96f41fd4812ac053a4d726e3bd2c906ea3dc87e2
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-23 12:09:28 -0400

    Criando e rodando um teste de integração com Spring Boot, JUnit e A...

--- projects/algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ projects/algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -1,11 +1,32 @@
 package com.algaworks.algafood;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
 @SpringBootTest
 class CadastroCozinhaIntegrationTests {
 
-
-
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+	
+	@Test
+	public void testarCadastroCozinhaComSucesso() {
+		// cenário
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+		
+		// ação
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+		
+		// validação
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+	
 }
