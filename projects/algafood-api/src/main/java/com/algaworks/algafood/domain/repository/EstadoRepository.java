f5584b8c7068431b3b21c8904a2ccc24f188c89d
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-24 11:11:53 -0400

    testing @RequestParam annotation

--- projects/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
+++ projects/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.algaworks.algafood.domain.model.Estado;
 
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Long> {
+
+	List<Estado> findByNomeContaining(String nome);
 
 }
